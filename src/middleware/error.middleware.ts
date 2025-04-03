1f11589d0d5a7ae5140ee8773b5780f7f9b082c0
huynhmanh219 <135082065+huynhmanh219@users.noreply.github.com>
2025-04-03 11:53:19 +0700

    fix code error

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -1,17 +1,16 @@
 import { NextFunction, Request, Response } from "express";
 import httpStatus from "../constants/httpStatus";
 import { omit } from "lodash";
-import { ErrorWithStatus } from "~/models/Errors";
+import { ErrorWithStatus } from "../models/Errors";
 export const defaultErrorHandler = (err:any,req:Request,res:Response,next:NextFunction)=>{
   if(err instanceof ErrorWithStatus){
     res.status(err.status).json(omit(err,['status']))
   }
-  
     Object.getOwnPropertyNames(err).forEach((key)=>{
     Object.defineProperty(err,key,{enumerable:true})
   })
 
-  res.status(err.status || httpStatus.INTERNAL_SERVER_ERROR).json({
+   res.status(err.status || httpStatus.INTERNAL_SERVER_ERROR).json({
     message:err.message,
     errorInfo:omit(err,['message'])
   })
